b7b9b5f56c2df1aaecd5142cbb87ec2667fdef95
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-03 20:29:24 -0500

    Add 'Add Number' Method

--- Assignment05_Yuan.py
+++ Assignment05_Yuan.py
@@ -8,9 +8,17 @@ class BasicMathOperation:
         self.__first_name = first_name
         self.__last_name = last_name
 
+    #Greet User Method
     def greet_user(self):
         print("Welcome, " + self.__first_name + " " + self.__last_name + ".\n")
 
+    #Add Number Method
+    def add_num(self):
+        num1 = int(input("Enter the 1st number:\n"))
+        num2 = int(input("Enter the 2nd number:\n"))
+        return print(num1 + num2)
+
 #-----------------------------------
 obj1 = BasicMathOperation()
-obj1.greet_user()
+#obj1.greet_user()
+obj1.add_num()
