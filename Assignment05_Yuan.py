87af80ccfa0cdc147dc546efc79937f2b6ac6d14
Rand-Sai <yuan.sa@northeastern.edu>
2024-03-03 20:38:20 -0500

    Add 'Perform Operation' method with error handling.

--- Assignment05_Yuan.py
+++ Assignment05_Yuan.py
@@ -17,8 +17,25 @@ class BasicMathOperation:
         num1 = int(input("Enter the 1st number:\n"))
         num2 = int(input("Enter the 2nd number:\n"))
         return print(num1 + num2)
+    
+    #Perform Operation Method
+    def perf_op(self, num1, num2, operator):
+        if operator == "+":
+            return print(num1 + num2)
+        elif operator == "-":
+            return print(num1 - num2)
+        elif operator == "*":
+            return print(num1 * num2)
+        elif operator == "/":
+            return print(num1 / num2)
+        else:
+            print("Sorry, you've enter a unknown operator, please try again. (+ - * / with quo. marks)")
+
 
 #-----------------------------------
 obj1 = BasicMathOperation()
 #obj1.greet_user()
-obj1.add_num()
+#obj1.add_num()
+obj1.perf_op(10, 20, "-")
+obj1.perf_op(3, 5, "*")
+obj1.perf_op(11, 2, "s")
